6648083293a5c3fc97bb4daf9d4b27f7ad321ec5
Vitor Medeiros <vitorpaulo3121@gmail.com>
2025-09-02 23:39:19 -0300

    method channel IOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,21 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller = window?.rootViewController as! FlutterViewController
+    let batteryChannel = FlutterMethodChannel(
+        name: "samples.flutter.dev/battery",
+        binaryMessenger: controller.binaryMessenger
+    )
+    
+    batteryChannel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
+        switch call.method {
+        case "getPlatformVersion":
+            self?.getPlatformVersion(result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
