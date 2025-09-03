4ead97af0dd72df0e4dc24fa138e8ec7ab8c4f0e
Vitor Medeiros <vitorpaulo3121@gmail.com>
2025-09-03 18:49:20 -0300

    Router Screen Implementada

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,10 +1,12 @@
 import 'package:app/data/services/platform_service.dart';
+import 'package:app/presentation/router/skill_router_delegate.dart';
 import 'package:get_it/get_it.dart';
 import 'data/services/logging_service.dart';
 
 final sl = GetIt.instance;
 
 void setupLocator() {
+  sl.registerSingleton<SkillRouterDelegate>(SkillRouterDelegate());
   sl.registerSingleton<LoggingService>(LoggingService());
   sl.registerSingleton<PlatformService>(PlatformService());
 }
