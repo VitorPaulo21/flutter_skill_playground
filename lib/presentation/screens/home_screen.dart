788a17ca5c6a79618c82241d8b79baff458428c6
Vitor Medeiros <vitorpaulo3121@gmail.com>
2025-09-03 22:24:48 -0300

    Ajustes Esteticos

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,64 +1,60 @@
 import 'package:app/domain/models/card_item.dart';
-import 'package:app/domain/components/grid_delegates/adaptative_grid_delegate.dart';
 import 'package:app/locator.dart';
 import 'package:app/presentation/router/skill_router_delegate.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   final List<CardItem> items = [
     CardItem(
       title: 'CustomPainter Screen',
       route: RouteNames.CUSTOM_PAINTER_SCREEN,
-      image: "https://i.ytimg.com/vi/kp14Y4uHpHs/maxresdefault.jpg",
+      image: "assets/custom-paint.jpg",
     ),
     CardItem(
       title: 'Router 2.0 Screen',
       route: RouteNames.ROUTER_2,
-      image: "https://i.ytimg.com/vi/_05zUd5Kdrw/sddefault.jpg",
+      image: "assets/navigator_2.jpg",
     ),
     CardItem(
       title: 'Bloc Screen',
       route: RouteNames.BLOC_STATE_SCREEN,
-      image:
-          "https://codeclusive.io/blog/introduction-to-flutter-bloc/images/bloc.png",
+      image: "assets/bloc.png",
     ),
     CardItem(
       title: 'Platform Channels Screen',
       route: RouteNames.PLATFORM_CHANNEL_SCREEN,
-      image:
-          "https://miro.medium.com/v2/resize:fit:1200/1*OJ2ebAo75tLVDKe0xOMeiA.png",
+      image: "assets/platform_channel.png",
     ),
     CardItem(
       title: 'Performance Screen',
       route: RouteNames.PERFORMANCE_SCREEN,
-      image: "https://i.ytimg.com/vi/cVAGLDuc2xE/maxresdefault.jpg",
+      image: "assets/performance.jpg",
     ),
     CardItem(
       title: 'Isolate Screen',
       route: RouteNames.ISOLATE_SCREEN,
-      image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwIrU6ie-cYV4SqQvTc_YZJEwC5rYy2BUfJg&s",
+      image: "assets/isolate.png",
     ),
     CardItem(
       title: 'AnimationController Screen',
       route: RouteNames.ANIMATION_SCREEN,
-      image: "https://miro.medium.com/1*kT7J_vQcySTcA1wnzle4tQ.gif",
+      image: "assets/animation.gif",
     ),
     CardItem(
       title: 'StreamBuilder Screen',
       route: RouteNames.STREAM_BUILDER_ERROR_SCREEN,
-      image: "https://i.ytimg.com/vi/MkKEWHfy99Y/maxresdefault.jpg",
+      image: "assets/stream_builder.jpg",
     ),
     CardItem(
       title: 'Slivers Screen',
       route: RouteNames.SLIVER_SCREEN,
-      image: "https://i.ytimg.com/vi/VhcxuShoW3k/maxresdefault.jpg",
+      image: "assets/sliver.jpg",
     ),
     CardItem(
       title: 'Dependency Injection',
       route: RouteNames.DEPENDENCY_INJECTION_SCREEN,
-      image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw5fh7HByQFptrygpBDV3dzCLx9TSkgVwyzg&s",
+      image: "assets/dependency_injection.png",
     ),
   ];
 
@@ -70,11 +66,14 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Flutter Skills Showcase')),
       body: GridView.builder(
         padding: EdgeInsets.all(15),
-        gridDelegate: AdaptativeGridDelegate(
-          minWidth: 100,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          mainAxisSpacing: 10,
           crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          childAspectRatio:
+              MediaQuery.of(context).orientation == Orientation.landscape
+              ? 1.778
+              : 1.3,
         ),
         itemCount: items.length,
         itemBuilder: (context, index) {
@@ -84,36 +83,41 @@ class HomeScreen extends StatelessWidget {
               borderRadius: BorderRadiusGeometry.circular(15),
             ),
             elevation: 5,
-            child: ClipRRect(
-              borderRadius: BorderRadiusGeometry.circular(15),
-              child: InkWell(
-                child: Stack(
-                  children: [
-                    Positioned.fill(
+            child: InkWell(
+              onTap: () {
+                _router.push(item.route);
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: ClipRRect(
+                      borderRadius: BorderRadiusGeometry.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                      ),
                       child: Hero(
                         tag: item.route.name,
-                        child: Image.network(item.image, fit: BoxFit.fitWidth),
+                        child: Image.asset(item.image, fit: BoxFit.fitWidth),
                       ),
                     ),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      left: 0,
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        alignment: Alignment.center,
-                        height: 40,
-                        color: Colors.black54,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: SizedBox(
+                      height: 23,
+                      child: FittedBox(
                         child: Text(
                           item.title,
                           textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white),
+                          style: GoogleFonts.oswald(
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
-                  ],
-                ),
-                onTap: () => _router.push(item.route),
+                  ),
+                ],
               ),
             ),
           );
